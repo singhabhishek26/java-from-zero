0ce8b19cfed9948ce42669373540df5d8f467c41
singhabhishek26 <abhi.thakur0026.as@gmail.com>
2025-11-02 20:26:32 +0530

    Completed Topic:Data Types

--- 1_Language_Fundamentals/1.3_Data_Types.java
+++ 1_Language_Fundamentals/1.3_Data_Types.java
@@ -59,9 +59,86 @@ Short data type is best with 16 bit processors (8085 and 8086) but these process
 ========================================================================================
 3. Int Data Type
 
-size:   4 bytes(32 bits)
+size:   4 bytes (32 bits)
 Range:  -2^31 to 2^31-1
 
 ex - int x = 2147483648 -- CE integer number too large.
 =========================================================================================
+4. Long Data Type
+
+size:   8 bytes (64 bits)
+Range:  -2^63 to 2^63-1
+=========================================================================================
+5. float Data Type (less accuracy i.e less decimal places)
+
+size:   4 byte (32 bits)
+Range:  -3.4e38 to 3.4e38
+=========================================================================================
+6. double Data type (more accuracy i.e more decimal places)
+
+size:   8 byte(64 bits)
+Range:  -1.7e308 to 1.7e308 = 1.7*10^38
+=========================================================================================
+7. Boolean Data type
+
+size:   Not Applicable (VM Dependency)
+Range:  N/A (But alowed values are true or false)
+
+ex-
+boolean b = True;  CE: Cannot find symbol Symbol: variable True location: class Test.
+
+int x= 0;
+if(x) {
+    SOP("Hello");
+}
+Above will result in compile time error: fount int required boolean.
+=========================================================================================
+8. char Data Type
+
+Size: 2 byte(16 bits)
+Range:0 to 65535
+
+# Old Language (C/C++) has char as 1 byte as -
+They were "ASCII Code based" and No of Allowed ASCII Code Charachters are 256
+a to z, A to Z, 0 to 9, $, #, +, -, *, .....
+And 8 bits (1 byte) are enough for 256 Chars.
+
+# Java Language
+Java is "Unicode Based" and no of different unicode charachters are >256 and <=65536
+To represent these charachters 8 bits are not enough hence we use 16 bits in JAVA i.e 2 bytes
+256 Charachters + 1 byte (Any symbol - ex - Tamil,Hindi,Chinese,etc.)
+<= 65536
+
+==========================================================================================
+
+
+SUMMARY OF JAVA PRIMITIVE DATA TYPES
+====================================
+
+DATA TYPE       SIZE    RANGE                   WRAPPER CLASS       DEFAULT VALUE
+===================================================================================================
+byte            1       -127 to 128             Byte                0
+short           2       -2^15 to 2^15-1         Short               0
+int             4       -2^31 to 2^31-1         Integer             0
+long            8       -2^63 to 2^63-1         Long                0
+float           4       -3.4e38 to 3.4e38       Float               0.0
+double          8       -1.7e308 to 1.7e308     Double              0.0
+char            2       0 to 65535              Charachter          0 (Represents Space Character).
+boolean         NA      NA (0, 1)               Boolean             false
+
+
+Note :- Null is he default value for object reference and we can't apply for primitives if we are trying to use for primitive we will get compile time error
+ex - char ch = null;
+CE: incompatible types
+    found nulltype
+    required char
 */
+
+class Data_Types {
+    public static void main(String[] args) {
+        boolean x = 0;
+        if (x) {
+            System.out.println("Hello");
+        }
+    }
+}
